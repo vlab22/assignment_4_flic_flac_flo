bd08f34feb3399feefdadd69fb9d442d2dac6fe4
Victor Leao <vlab22@gmail.com>
2021-04-06 10:05:37 +0200

    Update PlayerInfo.cs

--- shared/src/model/PlayerInfo.cs
+++ shared/src/model/PlayerInfo.cs
@@ -4,7 +4,18 @@
      * Empty placeholder class for the PlayerInfo object which is being tracked for each client by the server.
      * Add any data you want to store for the player here and make it extend ASerializable.
      */
-    public class PlayerInfo 
+    public class PlayerInfo : ASerializable
     {
+        public string userName;
+
+        public override void Serialize(Packet pPacket)
+        {
+            pPacket.Write(userName);
+        }
+
+        public override void Deserialize(Packet pPacket)
+        {
+            userName = pPacket.ReadString();
+        }
     }
 }
