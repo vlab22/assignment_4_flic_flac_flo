82a435f252a50994c4af6737eeabeb35016036cd
Victor Leao <vlab22@gmail.com>
2021-04-06 18:43:07 +0200

    Create WhoAmIResponse.cs

---
+++ shared/src/protocol/WhoAmIResponse.cs
@@ -0,0 +1,20 @@
+﻿namespace shared
+{
+    public class WhoAmIResponse : ASerializable
+    {
+        public int idInRoom;
+        public string userName;
+        
+        public override void Serialize(Packet pPacket)
+        {
+            pPacket.Write(idInRoom);
+            pPacket.Write(userName);
+        }
+
+        public override void Deserialize(Packet pPacket)
+        {
+            idInRoom = pPacket.ReadInt();
+            userName = pPacket.ReadString();
+        }
+    }
+}
