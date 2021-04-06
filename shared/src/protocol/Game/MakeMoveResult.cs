72a0b05c7bc3706a0739bd5ab3ec511798e38b89
Victor Leao <vlab22@gmail.com>
2021-04-06 15:41:48 +0200

    remove username field

--- shared/src/protocol/Game/MakeMoveResult.cs
+++ shared/src/protocol/Game/MakeMoveResult.cs
@@ -7,20 +7,17 @@
     {
         public int whoMadeTheMove;
         public TicTacToeBoardData boardData;
-        public string userName;
 
         public override void Serialize(Packet pPacket)
         {
             pPacket.Write(whoMadeTheMove);
             pPacket.Write(boardData);
-            pPacket.Write(userName);
         }
 
         public override void Deserialize(Packet pPacket)
         {
             whoMadeTheMove = pPacket.ReadInt();
             boardData = pPacket.Read<TicTacToeBoardData>();
-            userName = pPacket.ReadString();
         }
     }
 }
