ef04316eb5ea2206726392ea15e7e69d1c13f890
Victor Leao <vlab22@gmail.com>
2021-04-08 23:21:50 +0200

    format

--- client/Assets/Scripts/fsm/core/ApplicationState.cs
+++ client/Assets/Scripts/fsm/core/ApplicationState.cs
@@ -7,39 +7,39 @@ using UnityEngine;
  * 
  * Each state has access to the finite state machine (fsm) it is a part of, so it can switch state and communicate.
  */
-public abstract class ApplicationState : MonoBehaviour 
+public abstract class ApplicationState : MonoBehaviour
 {
-	//provides access to the fsm for subclasses
-	protected ApplicationFSM fsm			{ get; private set; }       
+    //provides access to the fsm for subclasses
+    protected ApplicationFSM fsm { get; private set; }
 
-	/**
+    /**
 	 * Provide a handle to the FSM to this state.
 	 */
-	public virtual void Initialize(ApplicationFSM pApplicationFSM)
-	{
-		fsm = pApplicationFSM;
-		gameObject.SetActive(false);
-	}
+    public virtual void Initialize(ApplicationFSM pApplicationFSM)
+    {
+        fsm = pApplicationFSM;
+        gameObject.SetActive(false);
+    }
 
-	/**
+    /**
 	 * Tells this state to enable itself.
 	 */
-	public virtual void EnterState()
-	{
-		Debug.Log("Entering application state " + this);
-		gameObject.SetActive(true);
-	}
+    public virtual void EnterState()
+    {
+        Debug.Log("Entering application state " + this);
+        gameObject.SetActive(true);
+    }
 
-	/**
+    /**
 	 * Tells this state to disable itself.
 	 */
-	public virtual void ExitState()
-	{
-		Debug.Log("Exiting application state " + this);
-		gameObject.SetActive(false);
-	}
+    public virtual void ExitState()
+    {
+        Debug.Log("Exiting application state " + this);
+        gameObject.SetActive(false);
+    }
 
-	/**
+    /**
 	 * Receives all messages from the server and calls the ABSTRACT handleNetworkMessage.
 	 * 
 	 * This method needs to be called EXPLICITLY (for example in the Update loop),
@@ -47,27 +47,26 @@ public abstract class ApplicationState : MonoBehaviour
 	 * 
 	 * Implement handleNetworkMessage in your subclass to actually handle the received message.
 	 */
-	virtual protected void receiveAndProcessNetworkMessages()
-	{
-		if (!fsm.channel.Connected)
-		{
-			Debug.LogWarning("Trying to receive network messages, but we are no longer connected.");
-			return;
-		}
+    virtual protected void receiveAndProcessNetworkMessages()
+    {
+        if (!fsm.channel.Connected)
+        {
+            Debug.LogWarning("Trying to receive network messages, but we are no longer connected.");
+            return;
+        }
 
-		//while there are messages, we have no issues AAAND we haven't been disabled (important!!):
-		//we need to check for gameObject.activeSelf because after sending a message and switching state,
-		//we might get an immediate reply from the server. If we don't add this, the wrong state will be processing the message
-		while (fsm.channel.HasMessage() && gameObject.activeSelf)
-		{
-			ASerializable message = fsm.channel.ReceiveMessage();
-			handleNetworkMessage(message);
-		}
-	}
+        //while there are messages, we have no issues AAAND we haven't been disabled (important!!):
+        //we need to check for gameObject.activeSelf because after sending a message and switching state,
+        //we might get an immediate reply from the server. If we don't add this, the wrong state will be processing the message
+        while (fsm.channel.HasMessage() && gameObject.activeSelf)
+        {
+            ASerializable message = fsm.channel.ReceiveMessage();
+            handleNetworkMessage(message);
+        }
+    }
 
-	/**
+    /**
 	 * Override/implement in a subclass
 	 */
-	abstract protected void handleNetworkMessage(ASerializable pMessage);
-
+    protected abstract void handleNetworkMessage(ASerializable pMessage);
 }
